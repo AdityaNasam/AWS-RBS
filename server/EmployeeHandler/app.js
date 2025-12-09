c0dfb1cc5864e226d6e49fe51b512b64c97b4542
Aditya Nasam <adityanasam@gmail.com>
2025-12-08 21:15:09 -0800

    bug fixes

--- server/EmployeeHandler/app.js
+++ server/EmployeeHandler/app.js
@@ -26,6 +26,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   // res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "PUT, DELETE");
   next();
 });
 
